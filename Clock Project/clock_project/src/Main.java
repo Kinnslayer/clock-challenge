1deb508f49664b525251153b0b380f2469bd8a46
Kinnslayer <rodrigo.s.alencar@hotmail.com>
2023-05-04 22:41:06 -0300

    adding validation on the inputs

--- Clock Project/clock_project/src/Main.java
+++ Clock Project/clock_project/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main (String[] args){
 
         Relogio anguloPonteiros = new Relogio();
-        
+           
         int resultadoAngulo = anguloPonteiros.retornaAnguloRelogio(Integer.parseInt(JOptionPane.showInputDialog(
         "Insira a(s) hora(s) do relógio entre 0 e 12: ")), Integer.parseInt(JOptionPane.showInputDialog(
         "Insira o(s) minuto(s) do relógio entre 0 e 59: ")));
--- Clock Project/clock_project/src/Relogio.java
+++ Clock Project/clock_project/src/Relogio.java
@@ -1,7 +1,17 @@
+import javax.swing.JOptionPane;
+
 public class Relogio {
 
     public int retornaAnguloRelogio (int hora, int minuto) {
-       
+
+        while (hora > 12 || hora < 0 || minuto > 59 || minuto < 0) {
+            JOptionPane.showMessageDialog(null, "Por favor insira um número válido");
+            hora = Integer.parseInt(JOptionPane.showInputDialog(
+                "Insira a(s) hora(s) do relógio entre 0 e 12: "));
+            minuto = Integer.parseInt(JOptionPane.showInputDialog(
+                "Insira o(s) minuto(s) do relógio entre 0 e 59: "));
+        } 
+            
         double horaAnguloPonteiro = (hora * 30) + (minuto * 0.5);
 
         double minutoAnguloPonteiro = minuto * 6;
